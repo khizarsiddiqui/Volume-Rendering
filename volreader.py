06018464597945a8cfe2ebdfd7289f3d2889f396
khizarsiddiqui <khizarsiddiqui10@gmail.com>
2022-10-17 00:26:29 +0500

    first commit

---
+++ volreader.py
@@ -0,0 +1,9 @@
+# first commit
+# some of the topics covered in this project:
+# • Using GLSL for GPU computations
+# • Creating vertex and fragment shaders
+# • Representing 3D volumetric data and using the volume ray casting algorithm
+# • Using numpy arrays for 3D transformation matrices
+
+# There are various ways to render a 3D data set. In this project, you’ll use the volume ray casting method, 
+# which is an image-based rendering technique used to generate the final image from the 2D slice, pixel by pixel.
